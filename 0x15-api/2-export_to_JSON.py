87342a95a265db64a7267d15916ef0de758b5642
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-20 11:21:04 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,30 @@
+#!/usr/bin/python3
+""" Python to get data from API and converts it to JSON"""
 
+import csv
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+  USER_ID = sys.argv[1]
+  url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+  res = requests.get(url_to_user)
+  """DOCUMENT"""
+  USERNAME = res.json().get('username')
+  """DOCUMENTATIONS"""
+  url_to_task = url_to_user + '/todos'
+  res = requests.get(url_to_task)
+  tasks = res.json()
+  
+  dict_data = {USER_ID: []}
+  for task in tasks:
+    TASK_COMPLETED_STATUS = task.get('completed')
+    TASK_TITLE = task.get('title')
+    dict_data[USER_ID].append({
+                              "task": TASK_TITLE,
+                              "completed": TASK_COMPLETED_STATUS,
+                              "username": USERNAME})
+    """Print(dict_data)"""
+    with open('{}.json'.format(USER_ID), 'w') as f:
+      json.dump(dict_data, f)
