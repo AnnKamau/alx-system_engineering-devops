bfba3b51867cf56c3a829d5e0d491d8d2316b4af
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-03-06 10:55:30 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
-"""Contains the recurse function"""
+"""Contains recurse function"""
 import requests
 
-def fetch_hot_posts(subreddit, hot_list=[], after="", count=0):
-    """Fetches titles of all hot posts on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {"User-Agent": "Custom User Agent"}
 
-    params = {"after": after, "count": count, "limit": 100}
-
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for 4XX or 5XX status codes
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
-        return None
-
-    try:
-        data = response.json()["data"]
-        after = data.get("after")
-        count += data.get("dist")
-        for child in data.get("children"):
-            hot_list.append(child["data"]["title"])
-
-        if after is not None:
-            return fetch_hot_posts(subreddit, hot_list, after, count)
-        return hot_list
-    except Exception as e:
-        print("Error processing response:", e)
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
 
     results = response.json().get("data")
